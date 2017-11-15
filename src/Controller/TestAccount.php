b9ffd5e20101a2699ee3b13a56ab0c97e682a52f
Danny Dohmen <dannyd@ezcompany.nl>
2017-11-15 14:45:44 +0100

    Now you actualy log out / log in

--- src/Controller/TestAccount.php
+++ src/Controller/TestAccount.php
@@ -4,6 +4,7 @@ namespace Drupal\ezrole_tester\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
 use Symfony\Component\HttpFoundation\RedirectResponse;
+use Drupal\Core\Access\AccessResult;
 
 /**
  * Class AdyenTest.
@@ -16,33 +17,14 @@ class TestAccount extends ControllerBase {
   public function set() {
     $account_name = \Drupal::request()->get('account');
     $destination = \Drupal::request()->get('destination');
-    $original = \Drupal::request()->get('original');
-    $role = \Drupal::request()->get('role');
-
     $users = \Drupal::entityTypeManager()->getStorage('user')->loadByProperties([
       'name' => $account_name,
     ]);
 
-    if ($account_name == 'ez-anonymous') {
-      $users = [\Drupal::entityTypeManager()->getStorage('user')->load(0)];
-    }
-
     if (!empty($users)) {
-      if ($original == TRUE) {
-        unset($_SESSION['ezCurrentAccount']);
-        unset($_SESSION['ezTestAccount']);
-        $user = reset($users);
-        drupal_set_message(t('Succesfully switched to %user', ['%user' => $user->label()]));
-      }
-      else {
-        if (!isset($_SESSION['ezCurrentAccount'])) {
-          $_SESSION['ezCurrentAccount'] = \Drupal::currentUser()->id();
-        }
+      $user = reset($users);
 
-        $user = reset($users);
-        $_SESSION['ezTestAccount'] = $user;
-        drupal_set_message(t('Succesfully switched to %role', ['%role' => $role]));
-      }
+      $this->userLogout($user);
     }
     else {
       drupal_set_message('Something went wrong', 'notice');
@@ -50,6 +32,44 @@ class TestAccount extends ControllerBase {
 
     $response = new RedirectResponse($destination);
     return $response->send();
+  }
+
+  /**
+   * Logs the current user out.
+   */
+  private function userLogout($new_user) {
+    $user = \Drupal::currentUser();
+
+    \Drupal::logger('user')->notice('Session closed for %name.', ['%name' => $user->getAccountName()]);
+
+    \Drupal::moduleHandler()->invokeAll('user_logout', [$user]);
+
+    \Drupal::service('session_manager')->destroy();
+    $url = user_pass_reset_url($new_user);
+
+    $response = new RedirectResponse($url . '/login?destination=' . \Drupal::request()->get('destination'));
+    return $response->send();
+  }
+
+  /**
+   * Custom access function.
+   */
+  public function access() {
+    $access = FALSE;
+
+    $roles = user_roles();
+
+    foreach ($roles as $role => $row) {
+      if (\Drupal::currentUser()->getUsername() == 'ez-' . $role) {
+        $access = TRUE;
+      }
+    }
+
+    if (\Drupal::currentUser()->id() == 1 || $access == TRUE) {
+      return AccessResult::allowed();
+    }
+
+    return AccessResult::forbidden();
   }
 
 }
